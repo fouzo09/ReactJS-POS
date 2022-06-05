3ba8f88a7319aaf09a60ebdea04cd06087c81957
Mafouz DIALLO <mafouzdiallo@gmail.com>
2022-06-05 12:09:50 +0000

    update, delete, read post

--- api/gestion-posts/controllers/posts.js
+++ api/gestion-posts/controllers/posts.js
@@ -15,15 +15,21 @@ const createPost = (req, res)=>{
 
 
 const getPost = (req, res)=>{
-
+    Post.find({_id: req.params.postID})
+        .then((result)=>res.status(200).json(result))
+        .catch((error)=>res.status(500).json("Post non trouvé"));
 }
 
 const updatePost = (req, res)=>{
-
+    Post.findOneAndUpdate({_id: req.params.postID}, req.body)
+        .then((result)=>res.status(200).json(result))
+        .catch((error)=>res.status(500).json("Post non trouvé"));
 }
 
 const deletePost = (req, res)=>{
-
+    Post.findOneAndDelete({_id: req.params.postID})
+        .then((result)=>res.status(200).json(result))
+        .catch((error)=>res.status(500).json("Post non trouvé"));
 }
 
 module.exports = {
--- api/gestion-posts/routes.js
+++ api/gestion-posts/routes.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const {
         createPost,
-        getPosts
+        getPosts,
+        getPost,
+        updatePost,
+        deletePost
     } = require("./controllers/posts.js");
 
 const postRouter = express.Router();
 
 postRouter.get("/", getPosts);
 postRouter.post("/", createPost);
+postRouter.get("/:postID", getPost);
+postRouter.put("/:postID", updatePost);
+postRouter.delete("/:postID", deletePost)
 
 module.exports = postRouter;
